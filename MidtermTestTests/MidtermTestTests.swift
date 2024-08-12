d4896a778b41383ac318973ee6c57c2f5ab2ec1b
KevinHung <kevinaugust145@gmail.com>
2024-08-12 15:07:01 +0800

    Initial Commit

---
+++ MidtermTestTests/MidtermTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  MidtermTestTests.swift
+//  MidtermTestTests
+//
+//  Created by 洪宗燦 on 2024/8/12.
+//
+
+import Testing
+@testable import MidtermTest
+
+struct MidtermTestTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
